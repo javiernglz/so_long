399ac099394423cd8731919bfc7b5a5d05b8d9eb
frnavarr <frnavarr@student.42malaga.com>
2024-12-28 00:51:33 +0100

    main.c

---
+++ src/main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frnavarr <frnavarr@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/28 00:45:43 by frnavarr          #+#    #+#             */
+/*   Updated: 2024/12/28 00:45:50 by frnavarr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mlx.h"
+#include <stdio.h>
+
+int main(void)
+{
+    void *mlx;
+    void *win;
+
+    mlx = mlx_init();
+    if (!mlx)
+        return (1);
+    win = mlx_new_window(mlx, 800, 600, "so_long");
+    if (!win)
+        return (1);
+    mlx_loop(mlx);
+    return (0);
+}
