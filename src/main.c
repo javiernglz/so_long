e2e1cb0090e51c6e505edce5b21a59ce0133444e
frnavarr <frnavarr@student.42malaga.com>
2025-01-15 00:39:06 +0100

    created window's constants

--- src/main.c
+++ src/main.c
@@ -6,22 +6,22 @@
 /*   By: frnavarr <frnavarr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 00:45:43 by frnavarr          #+#    #+#             */
-/*   Updated: 2025/01/15 00:36:15 by frnavarr         ###   ########.fr       */
+/*   Updated: 2025/01/15 00:38:35 by frnavarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
-#include <stdio.h>
 
 int main(void)
 {
     void *mlx;
     void *win;
-
+	
+	//initialize MinilibX
     mlx = mlx_init();
     if (!mlx)
         return (1);
-    win = mlx_new_window(mlx, 800, 600, "so_long");
+    win = mlx_new_window(mlx, WIN_WIDTH, WIN_HEIGHT, "so_long");
     if (!win)
         return (1);
     mlx_loop(mlx);
