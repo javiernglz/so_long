167bb7b1f6b4a1f7ae56835a3a7e94f8c6ddf285
frnavarr <frnavarr@student.42malaga.com>
2025-01-17 16:08:32 +0100

    init_game.c created

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: frnavarr <frnavarr@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 00:35:32 by frnavarr          #+#    #+#             */
-/*   Updated: 2025/01/15 00:35:47 by frnavarr         ###   ########.fr       */
+/*   Updated: 2025/01/17 16:07:55 by frnavarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,25 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+#ifdef
+#include "mlx.h"
+#endif
+
+# define TILE_SIZE 32 // Tamaño de cada tile en píxeles
+
+typedef struct s_game
+{
+	void	*mlx;
+	void	*win;
+	char	**map;
+	int		map_width;
+	int		map_height;
+	int		player_pos_x;
+	int		player_pos_y;
+	int		coins;
+	int		moves;
+}	t_game;
 
 #endif
